39033f8f8afd43fc009b83d12a2a22102c5b4790
gabriel654165 <gabriel.medoukali@epitech.eu>
2021-03-09 12:58:41 +0100

    [ADD] collisions, scripts animations, level design 1/3

---
+++ Assets/PLayer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PLayer : MonoBehaviour
+{
+    [Header("Horizontal Movement")]
+    public float moveSpeed = 10f;
+    public Vector2 direction;
+    private bool facingRight = true;
+    
+    [Header("Components")]
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    [Header("Physics")]
+    public float maxSpeed = 7f;
+
+
+    // Update is called once per frame
+    void Update() {
+        direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+    
+    void FixedUpdate() {
+        moveCharacter(direction.x);
+    }
+    
+    void moveCharacter(float horizontal) {
+        rb.AddForce(Vector2.right * horizontal * moveSpeed);
+        
+        animator.SetFloat("horizontal", Mathf.Abs(rb.velocity.x));
+
+        if ((horizontal > 0 && !facingRight) || (horizontal < 0 && facingRight)) {
+            Flip();
+        }
+        if (Mathf.Abs(rb.velocity.x) > maxSpeed) {
+            rb.velocity = new Vector2(Mathf.Sign(rb.velocity.x) * maxSpeed, rb.velocity.y);
+        }
+    }
+
+    void Flip() {
+        facingRight = !facingRight;
+        transform.rotation = Quaternion.Euler(0, facingRight ? 0 : 180,0);
+    }
+}
