b68358725061655ffb3eb5cea5a221540b103df4
gabriel MEDOUKALI <gabriel.medoukali@epitech.eu>
2021-03-26 19:40:49 +0100

    [ADD] stareship game

---
+++ Assets/History.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class History : MonoBehaviour
+{
+    [Header("Components")]
+    public PLayer hero;
+
+    [Header("Time")]
+    public float timeRemaining = 20;
+
+    [Header("History")]
+    public Text history;
+    private int lecture = 0;
+    private string[] text = {
+        "Hello !", "My name is Alexa, I'm the artificial inteligence of your starship.", "It seems that we arrive near to our destination : ", "The exoplanet 25468-id5551", "You are probably going to take the control of the starship, I'm not programmed to land proprely.", "Let's go on the control's room", 
+        "We have a techincal problem...", "The reactors control dosen't response and we are going to enter in the atmosphere of the exoplanet 25468-id5551", "You need to repaer the controller manually in order to take the control !!", "Go see at level -1 of the starship for some tools",
+        "Well it's perfect come to the conttrol pannel !",
+        "Oh no we have another problem, the artificial gravity doesn't work well we are to fast !!",
+        "WARNING : The starship is to heavy the reactors cannot stabilise the landing we are in a freefall", "You survival chances are 0.5%", "Good Bye"
+    };
+    private bool secPartHist = false;
+    private bool thirdPartHist = false;
+
+    [Header("Events")]
+    public bool turnOffGravity = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        history.text = "Hello !";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            if (lecture >= 5) {
+                if (hero.isCollidingEvent1) {
+                    secPartHist = true;
+                }
+            }
+            if (lecture >= 9) {
+                if (hero.isCollidingEvent2) {
+                   thirdPartHist = true;
+                }
+            }
+            if (lecture < 5) {
+                lecture += text[lecture] == "" ? 0 : 1;
+                history.text = text[lecture];
+            }
+            if (lecture >= 5 && secPartHist && lecture < 9) {
+                lecture += text[lecture] == "" ? 0 : 1;
+                history.text = text[lecture];
+            }
+            if (lecture >= 9 && thirdPartHist && lecture < 11) {
+                lecture += text[lecture] == "" ? 0 : 1;
+                history.text = text[lecture];
+            }
+            if (lecture >= 11 && lecture <= 14) {
+                lecture += text[lecture] == "" ? 0 : 1;
+                history.text = text[lecture];
+            }
+        }
+        if (lecture >= 11) {
+                turnOffGravity = true;
+                if (timeRemaining > 0)
+                    timeRemaining -= Time.deltaTime;
+                if (timeRemaining <= 0) {
+                    SceneManager.LoadScene("exo_planet");
+                }
+        }
+    }
+}
+//mettre un decompte en mode 3 2 1 end
+//le niveau sur l'exoplanete la faire en mode mario faire un long niveau et mettre un timer
--- Assets/PLayer.cs
+++ Assets/PLayer.cs
@@ -9,27 +9,93 @@ public class PLayer : MonoBehaviour
     public Vector2 direction;
     private bool facingRight = true;
     
+    [Header("Vertical Movement")]
+    public float jumpSpeed = 5f;
+    public float jumDelay = 0.25f;
+    public float jumpTimer;
+    public float distance;
+
     [Header("Components")]
     public Rigidbody2D rb;
     public Animator animator;
+    public LayerMask groundLayer;
+    public GameObject characterHolder;
+    public LayerMask ladderLayer;
+    public History HistScript;
 
     [Header("Physics")]
     public float maxSpeed = 7f;
+    public float linearDrag = 4f;
+    public float gravity = 1f;
+    public float fallMultiplier = 5f;
+
+    [Header("Collision")]
+    public bool onGround = false;
+    public float groundLength = 1.1f;
+    public Vector3 colliderOffset;
+    public bool isClimbing;
+    public bool isCollidingEvent1;
+    public bool isCollidingEvent2;
 
 
     // Update is called once per frame
     void Update() {
+        bool wasOnGround = onGround;
+        onGround = Physics2D.Raycast(transform.position + colliderOffset, Vector2.down, groundLength, groundLayer) || Physics2D.Raycast(transform.position - colliderOffset, Vector2.down, groundLength, groundLayer);
+        
+        if (Input.GetButtonDown("Jump")) {
+            jumpTimer = Time.time + jumDelay;
+        }
         direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
     }
-    
     void FixedUpdate() {
         moveCharacter(direction.x);
+        if (jumpTimer > Time.time && onGround) {
+            Jump();
+        }
+        /* SCALE */
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.up, distance, ladderLayer);
+        if (hitInfo.collider != null) {
+            if (Input.GetKeyDown(KeyCode.UpArrow)) {
+                isClimbing = true;
+            }
+        } else  {
+            isClimbing = false;
+        }
+        if (isClimbing) {
+            float inputVertical = Input.GetAxisRaw("Vertical");
+            rb.velocity = new Vector2(rb.velocity.x, inputVertical * maxSpeed);
+            rb.gravityScale = 0;
+        }
+        /* ZERO GRAVITY */
+        if (HistScript.turnOffGravity) {
+            float inputVertical = Input.GetAxisRaw("Vertical");
+            rb.velocity = new Vector2(rb.velocity.x, inputVertical * maxSpeed);
+            rb.gravityScale = 0;
+        }
+        modifyPhysics();
     }
-    
+    /* EVENTS */
+    void OnCollisionEnter2D(Collision2D col) {
+        if (col.gameObject.tag == "Event1") {
+            isCollidingEvent1 = true;
+        } else {
+            isCollidingEvent1 = false;
+        }
+        if (col.gameObject.tag == "Event2") {
+            isCollidingEvent2 = true;
+        } else {
+            isCollidingEvent2 = false;
+        }
+        if (col.gameObject.tag == "EventLoop") {
+            transform.position = new Vector3(transform.position.x + 75, transform.position.y, transform.position.z);
+        }
+        if (col.gameObject.tag == "EventLoop2") {
+            transform.position = new Vector3(transform.position.x - 75, transform.position.y, transform.position.z);
+        }
+    }
     void moveCharacter(float horizontal) {
         rb.AddForce(Vector2.right * horizontal * moveSpeed);
-        
-        animator.SetFloat("horizontal", Mathf.Abs(rb.velocity.x));
 
         if ((horizontal > 0 && !facingRight) || (horizontal < 0 && facingRight)) {
             Flip();
@@ -37,10 +103,45 @@ public class PLayer : MonoBehaviour
         if (Mathf.Abs(rb.velocity.x) > maxSpeed) {
             rb.velocity = new Vector2(Mathf.Sign(rb.velocity.x) * maxSpeed, rb.velocity.y);
         }
+        animator.SetFloat("vertical", rb.velocity.y);
+        animator.SetFloat("horizontal", Mathf.Abs(rb.velocity.x));
     }
 
+    void Jump() {
+        rb.velocity = new Vector2(rb.velocity.x, 0);
+        rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+        jumpTimer = 0;
+    }
+    
+    void modifyPhysics(){
+        bool changingDirections = (direction.x > 0 && rb.velocity.x < 0) || (direction.x < 0 && rb.velocity.x > 0);
+
+        if (onGround) {
+            if (Mathf.Abs(direction.x) < 0.4f || changingDirections) {
+                rb.drag = linearDrag;
+            } else {
+                rb.drag = 0f;
+            }
+            rb.gravityScale = 0;
+        } else {
+            rb.gravityScale = gravity;
+            rb.drag = linearDrag * 0.15f;
+            if (rb.velocity.y < 0) {
+                rb.gravityScale = gravity * fallMultiplier;
+            } else if (rb.velocity.y > 0 && !Input.GetButton("Jump")) {  //planete avec moins de gravité
+                rb.gravityScale = gravity * (fallMultiplier / 2);
+            }
+        }
+
+    }
     void Flip() {
         facingRight = !facingRight;
         transform.rotation = Quaternion.Euler(0, facingRight ? 0 : 180,0);
+    }
+
+    void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position + colliderOffset, transform.position + colliderOffset + Vector3.down * groundLength);
+        Gizmos.DrawLine(transform.position - colliderOffset, transform.position - colliderOffset + Vector3.down * groundLength);
     }
 }
